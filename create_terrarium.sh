d7e0de16c6427c9e15f8244863ecf3b14669072f
Andrew Calcutt <acalcutt@techidiots.net>
2024-09-03 13:55:22 -0400

    Update create_terrarium.sh

--- create_terrarium.sh
+++ create_terrarium.sh
@@ -12,16 +12,16 @@ vrtfile2=${OUTPUT_DIR}/swissalti3d_terrarium0-17_warp.vrt
 
 #rm rio/*
 ulimit -s 65536
-#gdalbuildvrt -overwrite ${vrtfile} ${INPUT_DIR}/*.tif
-#gdalwarp -r cubicspline -t_srs EPSG:3857 -dstnodata 0 -co COMPRESS=DEFLATE ${vrtfile} ${vrtfile2}
+gdalbuildvrt -overwrite ${vrtfile} ${INPUT_DIR}/*.tif
+gdalwarp -r cubicspline -t_srs EPSG:3857 -dstnodata 0 -co COMPRESS=DEFLATE ${vrtfile} ${vrtfile2}
 rio rgbify -e terrarium --min-z 0 --max-z 17 -j 24 --format png ${vrtfile2} ${mbtiles}
 
 #sqlite3 ${mbtiles} 'CREATE UNIQUE INDEX tile_index on tiles (zoom_level, tile_column, tile_row);'
-#sqlite3 ${mbtiles} 'UPDATE metadata SET value = "swissalti3d_terrarium_Z12-15" WHERE name = "name" AND value = "";'
-#sqlite3 ${mbtiles} 'UPDATE metadata SET value = "JAXA ALOS World 3D 30m (AW3D30) converted with rio rgbify" WHERE name = "description";'
-#sqlite3 ${mbtiles} 'UPDATE metadata SET value = "png" WHERE name = "format";'
-#sqlite3 ${mbtiles} 'UPDATE metadata SET value = "1" WHERE name = "version";'
-#sqlite3 ${mbtiles} 'UPDATE metadata SET value = "baselayer" WHERE name = "type";'
-#sqlite3 ${mbtiles} "INSERT INTO metadata (name,value) VALUES('attribution','<a href=""https://earth.jaxa.jp/en/data/policy/"">AW3D30 (JAXA)</a>');"
+sqlite3 ${mbtiles} 'UPDATE metadata SET value = "swissalti3d_terrarium_Z12-15" WHERE name = "name" AND value = "";'
+sqlite3 ${mbtiles} 'UPDATE metadata SET value = "JAXA ALOS World 3D 30m (AW3D30) converted with rio rgbify" WHERE name = "description";'
+sqlite3 ${mbtiles} 'UPDATE metadata SET value = "png" WHERE name = "format";'
+sqlite3 ${mbtiles} 'UPDATE metadata SET value = "1" WHERE name = "version";'
+sqlite3 ${mbtiles} 'UPDATE metadata SET value = "baselayer" WHERE name = "type";'
+sqlite3 ${mbtiles} "INSERT INTO metadata (name,value) VALUES('attribution','<a href=""https://earth.jaxa.jp/en/data/policy/"">AW3D30 (JAXA)</a>');"
 #sqlite3 ${mbtiles} 'PRAGMA journal_mode=DELETE;'
 
