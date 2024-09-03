0df953f1b8bc0915e415c5e955b5a7308a2fc1dd
Andrew Calcutt <acalcutt@techidiots.net>
2024-09-03 13:43:56 -0400

    Update create_terrainrgb.sh

--- create_terrainrgb.sh
+++ create_terrainrgb.sh
@@ -17,11 +17,11 @@ gdalwarp -r cubicspline -t_srs EPSG:3857 -dstnodata 0 -co COMPRESS=DEFLATE ${vrt
 rio rgbify -b -10000 -i 0.1 --min-z 0 --max-z 17 -j 24 --format png ${vrtfile2} ${mbtiles}
 
 #sqlite3 ${mbtiles} 'CREATE UNIQUE INDEX tile_index on tiles (zoom_level, tile_column, tile_row);'
-#sqlite3 ${mbtiles} 'UPDATE metadata SET value = "swissalti3d_terrainrgb_z0-Z17" WHERE name = "name" AND value = "";'
-#sqlite3 ${mbtiles} 'UPDATE metadata SET value = "JAXA ALOS World 3D 30m (AW3D30) converted with rio rgbify" WHERE name = "description";'
-#sqlite3 ${mbtiles} 'UPDATE metadata SET value = "png" WHERE name = "format";'
-#sqlite3 ${mbtiles} 'UPDATE metadata SET value = "1" WHERE name = "version";'
-#sqlite3 ${mbtiles} 'UPDATE metadata SET value = "baselayer" WHERE name = "type";'
-#sqlite3 ${mbtiles} "INSERT INTO metadata (name,value) VALUES('attribution','<a href=""https://earth.jaxa.jp/en/data/policy/"">AW3D30 (JAXA)</a>');"
+sqlite3 ${mbtiles} 'UPDATE metadata SET value = "swissalti3d_terrainrgb_z0-Z17" WHERE name = "name" AND value = "";'
+sqlite3 ${mbtiles} 'UPDATE metadata SET value = "JAXA ALOS World 3D 30m (AW3D30) converted with rio rgbify" WHERE name = "description";'
+sqlite3 ${mbtiles} 'UPDATE metadata SET value = "png" WHERE name = "format";'
+sqlite3 ${mbtiles} 'UPDATE metadata SET value = "1" WHERE name = "version";'
+sqlite3 ${mbtiles} 'UPDATE metadata SET value = "baselayer" WHERE name = "type";'
+sqlite3 ${mbtiles} "INSERT INTO metadata (name,value) VALUES('attribution','<a href=""https://earth.jaxa.jp/en/data/policy/"">AW3D30 (JAXA)</a>');"
 #sqlite3 ${mbtiles} 'PRAGMA journal_mode=DELETE;'
 
