c342b3d111fbeb37397c1a35e79a8246b23ff106
acalcutt <acalcutt@techidiots.net>
2024-10-12 11:42:00 -0400

    update scripts

--- create_terrainrgb.sh
+++ create_terrainrgb.sh
@@ -4,9 +4,9 @@
 
 INPUT_DIR=./download
 OUTPUT_DIR=./output
-vrtfile=${OUTPUT_DIR}/swissalti3d_terrainrgb_z0-Z17.vrt
-mbtiles=${OUTPUT_DIR}/swissalti3d_terrainrgb_z0-Z17.mbtiles
-vrtfile2=${OUTPUT_DIR}/swissalti3d_terrainrgb_z0-Z17_warp.vrt
+vrtfile=${OUTPUT_DIR}/swissalti3d_terrainrgb_z0-Z16.vrt
+mbtiles=${OUTPUT_DIR}/swissalti3d_terrainrgb_z0-Z16.mbtiles
+vrtfile2=${OUTPUT_DIR}/swissalti3d_terrainrgb_z0-Z16_warp.vrt
 
 [ -d "$OUTPUT_DIR" ] || mkdir -p $OUTPUT_DIR || { echo "error: $OUTPUT_DIR " 1>&2; exit 1; }
 
@@ -14,14 +14,14 @@ vrtfile2=${OUTPUT_DIR}/swissalti3d_terrainrgb_z0-Z17_warp.vrt
 ulimit -s 65536
 gdalbuildvrt -overwrite ${vrtfile} ${INPUT_DIR}/*.tif
 gdalwarp -r cubicspline -t_srs EPSG:3857 -dstnodata 0 -co COMPRESS=DEFLATE ${vrtfile} ${vrtfile2}
-rio rgbify -b -10000 -i 0.1 --min-z 0 --max-z 17 -j 24 --format png ${vrtfile2} ${mbtiles}
+rio rgbify -b -10000 -i 0.1 --min-z 0 --max-z 16 -j 24 --format webp ${vrtfile2} ${mbtiles}
 
 #sqlite3 ${mbtiles} 'CREATE UNIQUE INDEX tile_index on tiles (zoom_level, tile_column, tile_row);'
-sqlite3 ${mbtiles} 'UPDATE metadata SET value = "swissalti3d_terrainrgb_z0-Z17" WHERE name = "name" AND value = "";'
-sqlite3 ${mbtiles} 'UPDATE metadata SET value = "JAXA ALOS World 3D 30m (AW3D30) converted with rio rgbify" WHERE name = "description";'
-sqlite3 ${mbtiles} 'UPDATE metadata SET value = "png" WHERE name = "format";'
+sqlite3 ${mbtiles} 'UPDATE metadata SET value = "swissalti3d_terrainrgb_z0-Z16_webp" WHERE name = "name" AND value = "";'
+sqlite3 ${mbtiles} 'UPDATE metadata SET value = "swissALTI3D 2024 converted with rio rgbify" WHERE name = "description";'
+sqlite3 ${mbtiles} 'UPDATE metadata SET value = "webp" WHERE name = "format";'
 sqlite3 ${mbtiles} 'UPDATE metadata SET value = "1" WHERE name = "version";'
 sqlite3 ${mbtiles} 'UPDATE metadata SET value = "baselayer" WHERE name = "type";'
-sqlite3 ${mbtiles} "INSERT INTO metadata (name,value) VALUES('attribution','<a href=""https://earth.jaxa.jp/en/data/policy/"">AW3D30 (JAXA)</a>');"
+sqlite3 ${mbtiles} "INSERT INTO metadata (name,value) VALUES('attribution','<a href=https://www.swisstopo.admin.ch/en/height-model-swissalti3d>swissALTI3D</a>');"
 #sqlite3 ${mbtiles} 'PRAGMA journal_mode=DELETE;'
 
